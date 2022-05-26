5e431bd327c72a7832fa6a9187030d83888e514a
MattiDragon <matias.koivikko@gmail.com>
2022-05-26 12:19:04 +0300

    Fix datagen deleting readme

--- src/main/java/io/github/mattidragon/extendeddrawers/datagen/ReadmeDataProvider.java
+++ src/main/java/io/github/mattidragon/extendeddrawers/datagen/ReadmeDataProvider.java
@@ -6,6 +6,7 @@ import net.minecraft.data.DataProvider;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Objects;
 
 public class ReadmeDataProvider implements DataProvider {
     private static final String README = """
@@ -13,6 +14,8 @@ public class ReadmeDataProvider implements DataProvider {
             The data here is automatically generated and published only for reference for modpack and resource pack development.
             Any changes made to it will be reverted next time data generation is run.
             """;
+    private static final String HASH = SHA1.hashUnencodedChars(README).toString();
+    
     private final DataGenerator root;
     
     public ReadmeDataProvider(DataGenerator root) {
@@ -21,7 +24,13 @@ public class ReadmeDataProvider implements DataProvider {
     
     @Override
     public void run(DataCache cache) throws IOException {
-        Files.writeString(root.getOutput().resolve("README.md"), README);
+        var path = root.getOutput().resolve("README.md");
+        
+        if (!Objects.equals(cache.getOldSha1(path), HASH) || !Files.exists(path)) {
+            Files.createDirectories(path.getParent());
+            Files.writeString(path, README);
+        }
+        cache.updateSha1(path, HASH);
     }
     
     @Override
